0a734e745505d9fd62e2497cf9d206bd9abe20a1
Evkaaaaa <119289626+Evkaaaaa@users.noreply.github.com>
2024-03-01 09:11:24 +0300

    Add files via upload

---
+++ Progect/blocks/footer.php
@@ -0,0 +1,13 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-6 col-md">
+        <h5>О проекте</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">связь с разработчиком</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">группа в вк</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">инстаграмм</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">телеграмм канал</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
